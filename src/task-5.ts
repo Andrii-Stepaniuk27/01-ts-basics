a59b6d86b737e23fd9fcd09c3371d176924c6622
Andrii_Stepanuk <andreyka64125@gmail.com>
2026-06-05 20:53:02 +0300

    add all

---
+++ src/task-5.ts
@@ -0,0 +1,15 @@
+interface Status {
+  status: "loading" | "success" | "error";
+}
+
+function logStatus(status: Status) {
+  if (status.status === "loading") {
+    console.log("Loading...");
+  } else if (status.status === "success") {
+    console.log("Success!");
+  } else if (status.status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus({ status: "loading" });
